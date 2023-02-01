2c7a29878b7a00604f0f524fddb8c26826bf56ba
suzukikoki <114800671+suzukikoki@users.noreply.github.com>
2023-02-01 14:20:08 +0000

    2/1

---
+++ Chapter04.java
@@ -0,0 +1,10 @@
+public class Chapter04 {
+  public static void main(String[] args) {
+    String name = "yamada";
+    System.out.println(name);
+    name = "tanaka";
+    System.out.println(name);
+    int number = 1;
+    System.out.println(number);
+  }
+}
